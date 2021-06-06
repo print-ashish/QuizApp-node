1aeec45791c0989628b79c3d79a9dfdc34a7aae9
Ashish <as727243@gmail.com>
2021-06-06 20:29:35 +0530

    added css

--- public/js/user_score.js
+++ public/js/user_score.js
@@ -6,19 +6,26 @@ const players_section_id = document.getElementById("players_section_id");
 fetch('/xyz')
 .then(response => response.json()).then((data) => {
     // console.log(data);
-    var s = "";
-  for (let i = 0; i < data.players.length; i++) {
-    s = s+ ` 
-    <tr>
-    
-    <td>${data.players[i]}</td>
-    <td>${data.player_scores[i]}</td> 
-    </tr>`
-    
-    
+    if (data.players.length > 0) {
       
-  }
-
-  players.innerHTML = s;
+      var s = "";
+    for (let i = 0; i < data.players.length; i++) {
+      s = s+ ` 
+      <tr>
+      
+      <td>${data.players[i]}</td>
+      <td>${data.player_scores[i]}</td> 
+      </tr>`
+      
+      
+        
+    }
+  
+    players.innerHTML = s;
+    }
+    else{
+      players.innerHTML = "No players have answered your quiz share the link"
+    }
+    
   players_section_id.style.display = "block"
 })
